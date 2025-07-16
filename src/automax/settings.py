6d9cb180f0f0867ac4f83c8798fff0ab794152b1
Gandharv Kulkarni <114131432+gandharvk422@users.noreply.github.com>
2025-07-16 23:30:13 +0530

    automax-django-webapp

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -165,6 +166,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main', 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media Files (Uploaded Files)
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -183,7 +185,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 RESEND_API_KEY = env("RESEND_API_KEY")
 FROM_EMAIL = env("FROM_EMAIL")
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
