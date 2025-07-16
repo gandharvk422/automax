eff15d5ac8ee18d469c2e6c11b4cb81cdc5cc8b8
Gandharv Kulkarni <114131432+gandharvk422@users.noreply.github.com>
2025-07-16 22:56:12 +0530

    automax-django-webapp

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -161,9 +161,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'main', 'static')
 ]
 
 # Media Files (Uploaded Files)
