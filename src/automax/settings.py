4510bd7d5be26f654a1739a3084f9200e2042480
Gandharv Kulkarni <114131432+gandharvk422@users.noreply.github.com>
2025-07-16 22:41:01 +0530

    automax-django-webapp

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -13,9 +13,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import environ
 from pathlib import Path
-from distutils.util import strtobool
 
 from django.contrib.messages import constants as messages
+
+def strtobool(val):
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError(f"Invalid truth value: {val}")
 
 # Initialize Environ
 env = environ.Env()
