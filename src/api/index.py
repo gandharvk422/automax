d7ce8eaf9522475e9ceb797b6cecc924aed22bf9
Gandharv Kulkarni <114131432+gandharvk422@users.noreply.github.com>
2026-05-18 16:40:04 +0530

    Update sys.path insertion for module loading

--- src/api/index.py
+++ src/api/index.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automax.settings')
 
 from django.core.wsgi import get_wsgi_application
